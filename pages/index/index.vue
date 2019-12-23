3aef2f13a9afdd9e64aaf4bc3aec34a43090fcee
tarymee <tarymee@gmail.com>
2019-12-23 23:35:57 +0800

    up

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,23 @@
 <template>
-    <a class="start">
-        <image :src="img" mode="aspectFill" @click="index()"></image>
-        <div class="link1"></div>
-        <div class="link2"></div>
-        <div class="link3"></div>
-        <div class="link4"></div>
-        <div class="link5"></div>
+    <div class="start">
+        <image class="start-bg" :src="img" mode="aspectFill"></image>
+        <div class="top link1" @click="jump()"><image src="/static/img/top1.png" mode="aspectFill"></image></div>
+        <div class="top link2" @click="jump()"><image src="/static/img/top2.png" mode="aspectFill"></image></div>
+        <div class="top link3" @click="jump()"><image src="/static/img/top3.png" mode="aspectFill"></image></div>
+        <div class="top link4" @click="jump()"><image src="/static/img/top4.png" mode="aspectFill"></image></div>
+        <div class="top link5" @click="jump()"><image src="/static/img/top5.png" mode="aspectFill"></image></div>
 
-        <div class="link6"></div>
-        <div class="link7"></div>
-    </a>
+        <div class="mid link1" @click="jump()"><image src="/static/img/pro.png" mode="aspectFill"></image></div>
+        <div class="mid link2" @click="jump()"><image src="/static/img/pro2.png" mode="aspectFill"></image></div>
+
+        <div class="bot">
+            <div class="bot-item" @click="jump()"><image src="/static/img/nav1.png" mode="aspectFill"></image></div>
+            <div class="bot-item" @click="jump()"><image src="/static/img/nav2.png" mode="aspectFill"></image></div>
+            <div class="bot-item" @click="jump()"><image src="/static/img/nav3.png" mode="aspectFill"></image></div>
+            <div class="bot-item" @click="jump()"><image src="/static/img/nav4.png" mode="aspectFill"></image></div>
+            <div class="bot-item" @click="jump()"><image src="/static/img/nav5.png" mode="aspectFill"></image></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -18,11 +26,11 @@ export default {
     components: {},
     data() {
         return {
-            img: '/static/img/ad.png'
+            img: '/static/img/index.png'
         }
     },
     methods: {
-        index() {
+        jump(url) {
             u.jump('/pages/index1/index1', true)
         }
     },
@@ -55,74 +63,82 @@ export default {
     position: fixed;
     width: 750upx;
     height: 100%;
+    background-color: rgba(253,250,244);
 }
-.start image {
+.start-bg {
     display: block;
     width: 750upx;
-    height: 100%;
+    height: 1204upx;
 }
-.start .link1 {
+
+.top {
     position: absolute;
     width: 120upx;
-    height: 200upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 50upx;
+    height: 180upx;
+    top: 75upx;
+}
+.top image {
+    display: block;
+    width: 120upx;
+    height: 180upx;
+}
+.top.link1 {
     left: 70upx;
 }
 
-.start .link2 {
-    position: absolute;
-    width: 120upx;
-    height: 200upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 60upx;
+.top.link2 {
     left: 200upx;
 }
 
-.start .link3 {
-    position: absolute;
-    width: 120upx;
-    height: 200upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 60upx;
+.top.link3 {
     left: 330upx;
 }
 
-.start .link4 {
-    position: absolute;
-    width: 120upx;
-    height: 200upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 60upx;
+.top.link4 {
     left: 460upx;
 }
 
-.start .link5 {
-    position: absolute;
-    width: 120upx;
-    height: 200upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 60upx;
+.top.link5 {
     left: 580upx;
 }
 
 
 
-.start .link6 {
+.mid {
     position: absolute;
     width: 180upx;
     height: 300upx;
-    background-color: rgba(000,000,000,0.8);
+    /* background-color: rgba(000,000,000,0.8); */
     top: 300upx;
-    left: 340upx;
+}
+.mid.link1 {
+    left: 335upx;
+}
+.mid.link2 {
+    left: 535upx;
+}
+.mid image {
+    display: block;
+    width: 180upx;
+    height: 300upx;
 }
 
-.start .link7 {
+.bot {
     position: absolute;
-    width: 180upx;
-    height: 300upx;
-    background-color: rgba(000,000,000,0.8);
-    top: 300upx;
-    left: 540upx;
+    width: 750upx;
+    top: 950upx;
+    text-align: center;
 }
+.bot-item {
+    display: inline-block;
+    width: 125upx;
+    height: 225upx;
+    /* background-color: rgba(000,000,000,0.8); */
+}
+.bot image {
+    display: block;
+    width: 125upx;
+    height: 225upx;
+}
+
 </style>
